20e04f2eaea786933f4c2863e832fcd6f46f56d1
Guilherme Elias <guilherme.elias@gmail.com>
2012-06-28 11:45:29 -0300

    commit de validacao do objeto calculator

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -5,13 +5,17 @@ describe("Unit testing for Calculator", function() {
   	calculator = new Calculator();
   });
 
+  it("Should validate calculator was created", function() {
+   	expect(calculator).not.toBe(null);
+  });
+
   describe("Unit test for sum operation", function() {
   	it("should sum operands", function() {
     	var result = calculator.sum(1,1);
     	expect(result).toBe(2);
   	});
 
-  	it("should return null when sum letters", function() {
+  	it("Should validate result using equal", function() {
     	var result = calculator.sum(2,1);
     	expect(result).toEqual(3);
   	});		
